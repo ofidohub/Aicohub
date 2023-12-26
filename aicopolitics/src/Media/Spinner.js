fc3591969ca49ef04b3acf263f868cd8a5d82198
ofido250p <ofexfoundation@gmail.com>
2023-12-26 06:55:57 +0200

    commit -m "first commit"

--- aicopolitics/src/Media/Spinner.js
+++
@@ -1,9 +0,0 @@
-// Spinner.js
-import React from 'react';
-import './Spinner.css'; // Import the CSS file
-
-const Spinner = () => {
-  return <div className="spinner"></div>;
-};
-
-export default Spinner;
